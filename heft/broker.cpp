1b1597ef76439b24097e9f1a593b21b654c35ce8
mvrck19 <p.haidos@gmail.com>
2021-12-14 22:56:51 +0200

    works with table but no numbers show up

--- heft/broker.cpp
+++ heft/broker.cpp
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <iostream>
+#include <iterator>
 #include <string>
 #include <vector>
 
@@ -14,16 +15,16 @@ class Broker
     vector<Vm> vms;
     Workflow w;
 
+    const int comp_costs[3][10] = {{14, 13, 11, 13, 12, 13, 7, 5, 18, 21},
+                                   {16, 19, 13, 8, 13, 16, 15, 11, 12, 7},
+                                   {9, 18, 19, 17, 10, 9, 11, 14, 20, 16}};
+
     Broker(vector<Vm>& vms, Workflow& w)
     {
         this->vms = move(vms);
         this->w   = move(w);
         create_rank();
         rank_sort();
-    };
-
-    void give_comp_to_tasks(){
-        
     };
 
     void run()
@@ -33,7 +34,8 @@ class Broker
             cout << execution_times();
             if (task.dependancies_done())
             {
-                vms.at(find_least_execution_time(task)).assign(task);
+                // vms.at(find_least_execution_time(task)).assign(task);
+                assign(vms.at(find_least_execution_time(task)),task);
             }
         }
         cout << execution_times();
@@ -43,19 +45,43 @@ class Broker
     int find_least_execution_time(Task& task)
     {
         int index = 0;
-        int min   = vms.at(0).get_execution_time() + vms.at(0).task_execution_time(task);
+        // int min   = vms.at(0).get_execution_time() + vms.at(0).task_execution_time(task);
+        int min   = vms.at(0).get_execution_time() + get_cost(vms.at(0).id, task.id);
 
         for (int i = 1; i < vms.size(); i++)
         {
-            if (vms.at(i).task_execution_time(task) + vms.at(i).get_execution_time() < min)
+            // if (vms.at(i).task_execution_time(task) + vms.at(i).get_execution_time() < min)
+            if (get_cost(vms.at(i).id,task.id) + vms.at(i).get_execution_time() < min)
+
                 index = i;
         }
         return index;
     }
 
-    int comp_costs[3][10] = {{14, 13, 11, 13, 12, 13, 7, 5, 18, 21},
-                             {16, 19, 13, 8, 13, 16, 15, 11, 12, 7},
-                             {9, 18, 19, 17, 10, 9, 11, 14, 20, 16}};
+    int get_cost(int vm_id, int task_id)
+    {
+        return comp_costs[vm_id][task_id];
+    }
+
+    void assign(Vm vm,Task task)
+    {
+        // Calculate the execution time of the task on the vm
+        // Plus the communication cost
+        // TODO see if this works
+        if (task.prev.size() == 0)
+            vm.execution_time = vm.execution_time + transfer_time();
+        // execution_time = execution_time + task.mips / mips_capacity;
+        vm.execution_time = vm.execution_time + comp_costs[vm.id][task.id];
+        vm.exec.push_back(task);
+
+        // TODO dependancies must be done in this part of the code
+        task.setDone(true);
+    }
+
+    int transfer_time()
+    {
+        return 1;
+    }
 
     string execution_times()
     {
@@ -138,7 +164,8 @@ class Broker
         {
             for (auto&& task : w.tasks)
             {
-                sum = sum + (task.mips / vm.mips_capacity);
+                // sum = sum + (task.mips / vm.mips_capacity);
+                sum = sum + comp_costs[vm.id][task.id];
             }
         }
         return sum / vms.size();
--- heft/vm.cpp
+++ heft/vm.cpp
@@ -9,7 +9,6 @@ class Vm
     int id;
     int mips_capacity;
     int execution_time;
-    int comp_costs[3][10];
     vector<Task> exec;
 
     Vm(int id,int capacity){
@@ -33,37 +32,8 @@ class Vm
         this->execution_time = 0;
     };
 
-    void assign(Task& task)
-    {
-        // Calculate the execution time of the task on the vm
-        // Plus the communication cost
-        // TODO see if this works
-        if (task.prev.size() == 0)
-            execution_time = execution_time + transfer_time();
-        execution_time = execution_time + task.mips / mips_capacity;
-        exec.push_back(task);
-
-        // TODO dependancies must be done in this part of the code
-        task.setDone(true);
-    }
-
-    // TODO implement later if needed
-    int transfer_time()
-    {
-        return 1;
-    }
-
     int get_execution_time()
     {
         return execution_time;
     }
-
-    double task_execution_time(Task task)
-    {
-        return task.mips / this->mips_capacity;
-    }
-
-    // double task_execution_time(Task task)
-    // {
-    // }
 };
